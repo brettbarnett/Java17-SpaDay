96a0d2fc41ede2ec87cc78e0e32c83da3e41704e
Brett Barnett <brett_barnett@sbcglobal.net>
2024-05-13 21:17:20 -0500

    Completed Class 12 Studio: Model validation

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,19 +1,32 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.*;
+
 public class User {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 5, max = 15, message = "Username needs to have at least 5 characters and no less than 15 characters")
     private String username;
+
+    @Email(message = "Email needs to be in the correct format")
     private String email;
+
+    @Size(min = 6, message = "Password needs to have at least 6 characters")
     private String password;
+
+    @Size(min = 6, message = "Password needs to have at least 6 characters")
+    private String verify;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verify) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verify = verify;
     }
 
     public String getUsername() {
@@ -38,6 +51,22 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getVerify() {
+        return this.verify;
+    }
+
+
+
+    public void setVerify(String verify) {
+        this.verify = verify;
+        checkPassword();
+    }
+    private void checkPassword() {
+        if (password != null && verify != null && !password.equals(verify)) {
+            setVerify(null);
+        }
     }
 }
 
